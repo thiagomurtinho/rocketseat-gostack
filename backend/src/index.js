93b42acfb611fc052c17fcaef1948d0ec5e0643c
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-11-25 00:33:33 -0300

    feat: :sparkles: HTTP Methods and tests
    Configing HTTP methods examples and use insominia to test

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ msg: 'Hello GoStack' });
+app.get('/projects', (req, res) => {
+  return res.json(['project 1', 'project 2']);
 });
 
-app.listen(3333, () => console.log('Back-end started!'));
+app.post('/projects', (req, res) => {
+  return res.json(['project 1', 'project 2', 'project 3']);
+});
+
+app.put('/projects/:id', (req, res) => {
+  return res.json(['project 4', 'project 2', 'project 3']);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  return res.json(['project 2', 'project 3']);
+});
+
+app.listen(3333, () => console.log('>>> Back-end started!'));
