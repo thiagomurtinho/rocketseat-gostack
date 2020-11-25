9a63141ad181bd596f2b4b2d29958c0e366f74d8
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-11-25 01:15:23 -0300

    feat: :sparkles: Params types for HTTP quests
    Inplementing params for tests. Query, Route and Body

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,30 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (req, res) => {
+  const { title, owner } = req.query;
+
+  console.log(`title: ${title}`);
+  console.log(`owner: ${owner}`);
+
   return res.json(['project 1', 'project 2']);
 });
 
 app.post('/projects', (req, res) => {
+  const { title, owner } = req.body;
+
+  console.log(`title: ${title}`);
+  console.log(`owner: ${owner}`);
+
   return res.json(['project 1', 'project 2', 'project 3']);
 });
 
 app.put('/projects/:id', (req, res) => {
+  const params = req.params;
+
+  console.log(params);
   return res.json(['project 4', 'project 2', 'project 3']);
 });
 
