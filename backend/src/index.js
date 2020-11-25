bffeb97c5ae1af239d9d1d6752e7c515c6f23aea
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-11-24 23:35:20 -0300

    feat: :sparkles: Configurating nodemon
    Instaling and configurating nodemon to whatch the code changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.json({ msg: 'Hello GoStack!' });
+  return res.json({ msg: 'Hello GoStack' });
 });
 
-app.listen(3333);
+app.listen(3333, () => console.log('Back-end started!'));
