fd751f7159f155ec6f66d3b01ac90c5455ac3c89
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-11-30 01:01:41 -0300

    feat: :sparkles: State and immutability
    Implementing useState from React to control project variable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+  const [project, setProject] = useState([
+    'Desenvolviemento de App',
+    'Front-end web',
+  ]);
+
+  function handleAddProject() {
+    setProject([...project, `New project ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header title="React Title one">
-        <h3>Children</h3>
-      </Header>
-      <Header title="React Title two" />
+      <Header title="Projects" />
+      <ul>
+        {project.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddProject}>
+        Adicionar Projeto
+      </button>
     </>
   );
 }
