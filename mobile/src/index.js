08e5077e4f37e45d673011a48939cca264d32291
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-12-05 22:33:32 -0300

    feat: :sparkles: Listing API projects
    Installing and configuring Axios. Using useEffect useState to get items of api and save. Using FlatList to list on screen.

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
+
+import api from './services/api';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item }) => (
+            <Text style={styles.project}>{item.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 }
@@ -16,13 +39,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   title: {
     color: '#FFF',
     fontSize: 30,
     fontWeight: 'bold',
+  },
+  project: {
+    color: '#FFF',
+    fontSize: 30,
   },
 });
 
