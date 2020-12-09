70f6310b5276670d8aa8801e13b5e105e05afd06
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2020-12-09 00:33:12 -0300

    feat: :sparkles: NodeJs Debugging
    Using debugging tool of Vscode

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ msg: 'Hello World!' }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('✨ Server started on port 3333!');
