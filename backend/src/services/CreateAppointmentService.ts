3dde86743ed06debf840909d7cc4bbaf0f3dd992
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-03-02 00:08:41 -0300

    feat(backend): :sparkles: Registry creation
    Creation and implementation routes and service from users.

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -5,12 +5,15 @@ import AppointmentsRepository from '../repositories/AppointmentRepository';
 import appointmentsRouter from '../routes/appointments.routes';
 
 interface IRequestDTO {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: IRequestDTO): Promise<Appointment> {
+  public async execute({
+    provider_id,
+    date,
+  }: IRequestDTO): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -24,7 +27,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
