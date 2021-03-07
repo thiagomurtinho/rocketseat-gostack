1d3dd03c83591696e964946a491b8114f11374fe
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-03-07 17:56:02 -0300

    feat(backend): :sparkles: Files upload
    Implementation a new migration for avatar, adding and configure multer to packages and using multer on user routes.

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -29,5 +34,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ sucess: request.file });
+  }
+);
 
 export default usersRouter;
