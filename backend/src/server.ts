748cd8c206f77c004394748c0e5ca8757b819d70
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-04-21 23:00:07 -0300

    feat(backend): :lock: Enabiling CORS in the API
    Installing and adding CORS to routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
-import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import 'reflect-metadata';
+import cors from 'cors';
+
 import routes from './routes';
 
 import AppError from './errors/AppError';
@@ -9,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -28,9 +31,9 @@ app.use(
       status: 'error',
       message: 'Internal server error',
     });
-  }
+  },
 );
 
 app.listen(3333, () => {
-  console.log('✨ Server started on port 3333!');
+  console.log('🚀 Server started on port 3333!');
 });
