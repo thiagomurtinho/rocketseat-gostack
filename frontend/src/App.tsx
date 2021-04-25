37bf3d93aca6974452bc88f7714cf400b1ef5293
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-04-24 22:05:31 -0300

    feat(frontend): :sparkles: Toasts structure
    Toast components creating and implementating styles. Importing toast on App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,15 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
