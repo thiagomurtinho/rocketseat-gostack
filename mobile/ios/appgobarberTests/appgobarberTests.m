0c240f83aeace29c36a97d704bcda10b2ef17af2
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-05-04 01:32:57 -0300

    feat(mobile): :sparkles: Configuring structure
    React-native app installing. Eslint, Prettier and editorconfig installing too.

--- mobile/ios/appgobarberTests/appgobarberTests.m
+++ mobile/ios/appgobarberTests/appgobarberTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobileTests : XCTestCase
+@interface appgobarberTests : XCTestCase
 
 @end
 
-@implementation mobileTests
+@implementation appgobarberTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
