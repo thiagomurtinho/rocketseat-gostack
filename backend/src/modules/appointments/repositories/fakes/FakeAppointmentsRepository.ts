727bf132994a74089b6bc69b3d443878d84278f2
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-05-24 15:46:45 -0300

    test(backend): :white_check_mark: Scheduling tests
    Creating tests for schedulings rejected

--- backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,15 +1,18 @@
 import { v4 as uuid } from 'uuid';
+import { isEqual } from 'date-fns';
+
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
-import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../IAppointmentsRepository';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 export default class FakeAppointmentsRepository
   implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return findAppointment;
