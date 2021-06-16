5287fc06c0af1bbdd4d986e4ff0f9aac1bab8726
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-06-16 16:33:39 -0300

    feat(backend): :sparkles: Email template
    Creating provider for email template, refactoring user service of send mail. Using handlebars for template generate.

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,8 +5,6 @@ import IUsersRepository from '../repositories/IUsersRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
-// import User from '../infra/typeorm/entities/User';
-
 interface IRequest {
   email: string;
 }
@@ -33,10 +31,20 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokenRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido: ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}} segue token: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -6,12 +6,20 @@ import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
+container.registerInstance<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  new HandlebarsMailTemplateProvider(),
+);
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );
