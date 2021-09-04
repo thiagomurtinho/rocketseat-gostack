9e462a16a377dfdcaa3bac07cd9db1b586402009
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-09-03 22:59:08 -0300

    feat(backend): :sparkles: Schedule creation
    Implementing tests and service.

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,18 +1,12 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import AppointmentController from '../controller/AppointmentsController';
+import AppointmentsController from '../controller/AppointmentsController';
 
 const appointmentsRouter = Router();
-const appointmentsController = new AppointmentController();
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
-
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
 
 appointmentsRouter.post('/', appointmentsController.create);
 
