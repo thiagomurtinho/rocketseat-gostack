1a3ef0ad0b307452aaba37ab79a6d53858bbbc27
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-09-18 01:08:58 -0300

    feat(mobile): :sparkles: Creation structure
    Implementing basic components and styles for page of display Appointments.

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useAuth } from '../../hooks/auth';
+import {
+  BackButton,
+  Container,
+  Header,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointmet: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+
+  const route = useRoute();
+  const { goBack } = useNavigation();
+  const routeParams = route.params as RouteParams;
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  console.log(route.params);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabelereiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointmet;
