99366f2f32c5d54e2b179da1b35303c6d48b0bb9
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-09-18 22:41:22 -0300

    feat(mobile): :sparkles: Seeking API availability
    Implementing a useEffect function to access API and seeking provider availability

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -32,6 +32,11 @@ export interface Provider {
   avatar_url: string;
 }
 
+interface Availability {
+  hour: number;
+  available: boolean;
+}
+
 const CreateAppointmet: React.FC = () => {
   const { user } = useAuth();
 
@@ -39,6 +44,7 @@ const CreateAppointmet: React.FC = () => {
   const { goBack } = useNavigation();
   const routeParams = route.params as RouteParams;
 
+  const [availability, setAvailability] = useState<Availability[]>([]);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [providers, setProviders] = useState<Provider[]>([]);
@@ -49,6 +55,18 @@ const CreateAppointmet: React.FC = () => {
   useEffect(() => {
     api.get('providers').then(response => setProviders(response.data));
   }, []);
+
+  useEffect(() => {
+    api
+      .get(`providers/${selectedProvider}/day-availability`, {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate(),
+        },
+      })
+      .then(response => setAvailability(response.data));
+  }, [selectedDate, selectedProvider]);
 
   const navigateBack = useCallback(() => {
     goBack();
