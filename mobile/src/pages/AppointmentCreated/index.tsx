59e3a438b3a27fcd762ea70059390d763508b578
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-09-19 22:15:07 -0300

    feat(mobile): :sparkles: Creating screen structure
    Implementing components, styles and locic for confirm and redirect to dashboard

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import { useNavigation } from '@react-navigation/core';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Description,
+  OkButton,
+  OkButtonText,
+  Title,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>"Dia", dia "dia" de "mes" de "ano" as "horas".</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
