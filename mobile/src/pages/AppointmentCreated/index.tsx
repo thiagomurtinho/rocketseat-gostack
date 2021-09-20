7a3fafe69bcc8b23936b85ce682bb7b03340ff01
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-09-19 22:28:08 -0300

    feat(mobile): :sparkles: Formatting the date
    Implementing locic for receive params date and display formatted date on description

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,6 +1,8 @@
-import { useNavigation } from '@react-navigation/core';
-import React, { useCallback } from 'react';
-import { View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useCallback, useMemo } from 'react';
+import { format } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+
 import Icon from 'react-native-vector-icons/Feather';
 import {
   Container,
@@ -10,8 +12,15 @@ import {
   Title,
 } from './styles';
 
+interface RouteParams {
+  date: number;
+}
+
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
 
   const handleOkPressed = useCallback(() => {
     reset({
@@ -24,12 +33,20 @@ const AppointmentCreated: React.FC = () => {
     });
   }, [reset]);
 
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBr },
+    );
+  }, [routeParams.date]);
+
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
 
       <Title>Agendamento concluído</Title>
-      <Description>"Dia", dia "dia" de "mes" de "ano" as "horas".</Description>
+      <Description>{formattedDate}</Description>
 
       <OkButton onPress={handleOkPressed}>
         <OkButtonText>Ok</OkButtonText>
